f131f4041c8c50883a03b52fd22889a2d6e3b79f
David <davidfloyd322@gmail.com>
2019-01-27 17:00:37 +0200

    fix/server: fixing eslint errors

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
+/* eslint-disable */
+
 const express = require('express');
-const fileUpload = require('express-fileupload');;
+const fileUpload = require('express-fileupload');
 const app = express();
 
-app.use('/form', express.static(__dirname + '/index.html'));
-app.use('/progress.js', express.static(__dirname + '/progress.js'));
-app.use('/HttpRequest.js', express.static(__dirname + '/HttpRequest.js'));
-app.use('/files', express.static(__dirname + '/uploads'));
+app.use('/form', express.static(`${__dirname}/index.html`));
+app.use('/progress.js', express.static(`${__dirname}/src/progress.js`));
+app.use('/HttpRequest.js', express.static(`${__dirname}/src/HttpRequest.js`));
+app.use('/files', express.static(`${__dirname}/uploads`));
 
 // default options
 app.use(fileUpload());
 
 app.post('/ping', function(req, res) {
+  res.send('pong');
+});
+app.get('/ping', function(req, res) {
   res.send('pong');
 });
 
@@ -27,14 +32,14 @@ app.post('/upload', function(req, res) {
 
   sampleFile = req.files.sampleFile;
 
-  uploadPath = __dirname + '/uploads/' + sampleFile.name;
+  uploadPath = `${__dirname}/uploads/${sampleFile.name}`;
 
   sampleFile.mv(uploadPath, function(err) {
     if (err) {
       return res.status(500).send(err);
     }
 
-    res.send('File uploaded to ' + uploadPath);
+    res.send(`File uploaded to ${uploadPath}`);
   });
 });
 
