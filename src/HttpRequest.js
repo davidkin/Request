93f3c5cfe15ba7d2c1e2c3c41131c1ca33f63aac
David <davidfloyd322@gmail.com>
2019-01-27 17:04:58 +0200

    feature/get: implements  method "GET"

--- src/HttpRequest.js
+++ src/HttpRequest.js
@@ -1,106 +1,136 @@
+function setHeader(download, headers) {
+  for (const key in headers) {
+    download.setRequestHeader(key, headers[key]);
+  }
+}
+
+function createURL(baseUrl, url, params) {
+  const finalUrl = new URL(baseUrl + url);
+
+  for (const key in params) {
+    finalUrl.searchParams.set(key, params[key]);
+  }
+
+  return finalUrl;
+}
+
+const elementsAreFunction = arrayOfFunction => arrayOfFunction.every(value => typeof value === 'function');
+
+
 class HttpRequest {
-  // get request options({ baseUrl, headers })
   constructor({ baseUrl, headers }) {
     this.baseUrl = baseUrl;
     this.headers = headers;
   }
 
   get(url, config) {
-    const finalUrl = this.baseUrl + url;
-    const { headers, params, mode, cache } = config;
+    const xhr = new XMLHttpRequest();
+
+    const { transformResponse, headers, params, responseType, onDownloadProgress } = config;
+    const finalUrl = createURL(this.baseUrl, url, params);
+
+    setHeader(xhr, this.headers);
 
     return new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest();
-
       xhr.open('GET', finalUrl, true);
-      xhr.send();
+      xhr.responseType = responseType;
 
-      xhr.onreadystatechange = () => {
+      setHeader(xhr, headers);
+
+      if (onDownloadProgress) {
+        onDownloadProgress(xhr, 'download');
+      }
+
+      xhr.onload = () => {
         if (xhr.status !== 200) {
           return reject(xhr.status);
         }
-        resolve(xhr.responseText);
+
+        return transformResponse === undefined || !elementsAreFunction(transformResponse)
+          ? resolve(xhr.response)
+          : resolve(transformResponse.reduce((acc, func) => func(acc), xhr.response), null);
       };
+
+      xhr.send();
     });
   }
 
-  // post(url, config) {
-  //   return null;
-  // }
+  post(url, config) {
+
+  }
 }
 
-/*
-const reuest = new HttpRequest({
-  baseUrl: 'http://localhost:8000'
-});
+// const config = {
 
-reuest.get('/user/12345', { headers: { contentType: undefined } })
-  .then(response => {
-    console.log(response);
-  })
-  .catch(e => {
-    console.log(e);
-  });
+// `transformResponse` allows changes to the response data to be made before
+// it is passed to then/catch
+// transformResponse: [function(data) {
+// Do whatever you want to transform the data
+
+//   return data;
+// }],
+
+// `headers` are custom headers to be sent
+// headers: { 'X-Requested-With': 'XMLHttpRequest' },
+
+// `params` are the URL parameters to be sent with the request
+// Must be a plain object or a URLSearchParams object
+// params: {
+//   ID: 12345
+// },
+// `data` is the data to be sent as the request body
+// Only applicable for request methods 'PUT', 'POST', and 'PATCH'
+// When no `transformRequest` is set, must be of one of the following types:
+// - string, plain object, ArrayBuffer, ArrayBufferView, URLSearchParams
+// - Browser only: FormData, File, Blob
+// - Node only: Stream, Buffer
+
+// data: {
+//   firstName: 'Fred'
+// },
+
+// `responseType` indicates the type of data that the server will respond with
+// options are 'arraybuffer', 'blob', 'document', 'json', 'text', 'stream'
+// responseType: 'text' // default
+
+// `onUploadProgress` allows handling of progress events for uploads
+// onUploadProgress: progressEvent => {}
+
+// `onDownloadProgress` allows handling of progress events for downloads
+// onDownloadProgress(progressEvent) {
+//   // Do whatever you want with the native progress event
+// }
+
+// };
+
+// const request = new HttpRequest({
+//   baseUrl: 'http://localhost:8000'
+// });
+
+// request.get('/form', config)
+// .then(response => {
+//   console.log(`Well done - ${response}`);
+// })
+// .catch(e => {
+//   console.log(e);
+// });
 
 
-reuest.get('/user/12345', { onDownloadProgress, headers: {contentType: undefined} })
-  .then(response => {
-    console.log(response);
-  })
-  .catch(e => {
-    console.log(e)
-  });
+// reuest.get('/form', { headers: {contentType: undefined} })
+// .then(response => {
+//   console.log(response);
+// })
+// .catch(e => {
+//   console.log(e)
+// });
 
-reuest.post('/save', { data: formdata, header, onUploadProgress })
-  .then(response => {
-    console.log(response);
-  })
-  .catch(e => {
-    console.log(e)
-  });
+// reuest.post('/upload', {
+//   data: form,
+// })
+// .then(response => {
+// console.log('---', `Well done - ${response}`);
+// })
+// .catch(e => {
+//   console.log(e)
+// });
 
-config = {
-
-  // `transformResponse` allows changes to the response data to be made before
-  // it is passed to then/catch
-  transformResponse: [function (data) {
-    // Do whatever you want to transform the data
-
-    return data;
-  }],
-
-  // `headers` are custom headers to be sent
-  headers: {'X-Requested-With': 'XMLHttpRequest'},
-
-  // `params` are the URL parameters to be sent with the request
-  // Must be a plain object or a URLSearchParams object
-  params: {
-    ID: 12345
-  },
-
-  // `data` is the data to be sent as the request body
-  // Only applicable for request methods 'PUT', 'POST', and 'PATCH'
-  // When no `transformRequest` is set, must be of one of the following types:
-  // - string, plain object, ArrayBuffer, ArrayBufferView, URLSearchParams
-  // - Browser only: FormData, File, Blob
-  // - Node only: Stream, Buffer
-
-  data: {
-    firstName: 'Fred'
-  },
-
-  // `responseType` indicates the type of data that the server will respond with
-  // options are 'arraybuffer', 'blob', 'document', 'json', 'text', 'stream'
-  responseType: 'json', // default
-
-  // `onUploadProgress` allows handling of progress events for uploads
-  onUploadProgress: function (progressEvent) {
-    // Do whatever you want with the native progress event
-  },
-
-  // `onDownloadProgress` allows handling of progress events for downloads
-  onDownloadProgress: function (progressEvent) {
-    // Do whatever you want with the native progress event
-  },
-}
-*/
