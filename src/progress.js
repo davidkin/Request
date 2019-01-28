220bf020a63ccc3b3db5acfc17bca39906778854
David <davidfloyd322@gmail.com>
2019-01-28 17:11:47 +0200

    developer: move "downloadFile" function to file downloadFile.js

--- src/progress.js
+++ src/progress.js
@@ -35,19 +35,6 @@ function getImgUrl(binaryImg) {
   return imageUrl;
 }
 
-function downloadFile(blob, fileName) {
-  const a = document.createElement('a');
-  const url = URL.createObjectURL(blob);
-
-  document.body.appendChild(a);
-  a.style = 'display: none';
-  a.href = url;
-  a.download = fileName;
-  a.click();
-
-  URL.revokeObjectURL(url);
-}
-
 const request = new HttpRequest({
   baseUrl: 'http://localhost:8000'
 });
