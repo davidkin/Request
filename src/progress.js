fc5b631bf97478dfe5f65b0cf641d6258d8d9142
David <davidfloyd322@gmail.com>
2019-01-28 14:27:44 +0200

    fix/progress: change all laguage to EN

--- src/progress.js
+++ src/progress.js
@@ -6,24 +6,29 @@ function loadBarProgress(nameOfBar, loaded, total) {
   progressBar.style.width = `${progress}%`;
 }
 
+function clearBarProgess (nameOfBar) {
+  const progressBar = document.querySelector(`#${nameOfBar}`);
+  progressBar.style.width = `${0}%`;
+}
+
 function events(xmlRequest, progressEvent) {
   if (progressEvent === 'upload') {
     xmlRequest.upload.onprogress = event => {
       loadBarProgress('upload-progress-bar', event.loaded, event.total);
-      console.log('---', `Загружено на сервер ${event.loaded} байт из ${event.total}`);
+      console.log('---', `Upload to the server ${event.loaded} bytes from ${event.total}`);
     };
 
     xmlRequest.upload.onload = () => {
-      console.log('---', 'Данные полностью загружены на сервер');
+      console.log('---', 'Data is fully uploaded to the server');
     };
 
     xmlRequest.upload.onerror = () => {
-      console.log('---', 'Произошла ошибка при загрузке данных на сервер!');
+      console.log('---', 'An error occurred while loading data to the server!');
     };
   } else if (progressEvent === 'download') {
     xmlRequest.onprogress = event => {
       loadBarProgress('download-progress-bar', event.loaded, event.total);
-      console.log(`Получено с сервера ${event.loaded} байт из ${event.total}`);
+      console.log(`Received from ${event.loaded} server bytes from ${event.total} `);
     };
   }
 }
@@ -74,6 +79,7 @@ document.getElementById('uploadForm').onsubmit = function(e) {
   })
     .then(response => {
       console.log('---', `Well done - ${response}`);
+      clearBarProgess('upload-progress-bar');
     })
     .catch(e => {
       console.log(e);
@@ -86,7 +92,8 @@ document.getElementById('downloadForm').onsubmit = function(e) {
 
   const dataOfFile = document.querySelector('.file-input').value;
   const img = document.querySelector('.img');
-  const isImg = dataOfFile.split('.')[1] === 'png' || dataOfFile.split('.')[1] === 'JPG' || dataOfFile.split('.')[1] === 'jpeg';
+  const isImg = dataOfFile.split('.')[1] === 'png' || dataOfFile.split('.')[1] === 'JPG' 
+            || dataOfFile.split('.')[1] === 'jpeg' || dataOfFile.split('.')[1] === 'jpg';
 
   request.get(`/files/${dataOfFile}`, {
     responseType: 'blob',
