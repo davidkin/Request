47e1581f55c41aaa54d482aaeba6711939929cf0
David <davidfloyd322@gmail.com>
2019-01-29 12:33:40 +0200

    fix/developer: add progress procent for a loadBar in width

--- src/progress.js
+++ src/progress.js
@@ -3,12 +3,11 @@
 function loadBarProgress(nameOfBar, loaded, total) {
   const progressBar = document.querySelector(`#${nameOfBar}`);
   const title = document.querySelector('title');
-  title.innerHTML =  `Document  ${Math.floor((loaded / total) * 100)}%`
+  
+  title.innerHTML =  `Document  ${Math.floor((loaded / total) * 100)}%`;
+  progressBar.style.width = `${Math.floor((loaded / total) * 100)}%`;
 
   if (loaded === total) {
-    progressBar.style.opacity = `1`;
-    progressBar.style.width = `100%`;
-
     setTimeout(() => {
       title.innerHTML =  `Document`;
       progressBar.style.opacity = `0`;
