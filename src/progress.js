dcbc52219f8c0dfb622b1247f8aea24edcaaabbe
David <davidfloyd322@gmail.com>
2019-01-29 16:29:10 +0200

    improve/fileList: add drag and drop function for showFile block

--- src/progress.js
+++ src/progress.js
@@ -48,6 +48,47 @@ document.querySelector('.file-input').onchange = function() {
   }
 };
 
+function dragAndDrop(elem) {
+  elem.onmousedown = function(e) {
+    const coords = getCoords(elem);
+    const shiftX = e.pageX - coords.left;
+    const shiftY = e.pageY - coords.top;
+
+    elem.style.position = 'absolute';
+    document.body.appendChild(elem);
+    moveAt(e);
+
+    elem.style.zIndex = 1000; // ��� ������� ����������
+
+    function moveAt(e) {
+      elem.style.left = `${e.pageX - shiftX}px`;
+      elem.style.top = `${e.pageY - shiftY}px`;
+    }
+
+    document.onmousemove = function(e) {
+      moveAt(e);
+    };
+
+    elem.onmouseup = function() {
+      document.onmousemove = null;
+      elem.onmouseup = null;
+    };
+  };
+
+  elem.ondragstart = function() {
+    return false;
+  };
+
+  function getCoords(el) {
+    const box = el.getBoundingClientRect();
+
+    return {
+      top: box.top + pageYOffset,
+      left: box.left + pageXOffset
+    };
+  }
+}
+
 document.querySelector('.getFile').onchange = function(e) {
   document.querySelector('.js-fileName').innerHTML = e.target.value.replace(/.*\\/, '');
 };
