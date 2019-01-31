900c516864e1be036e5f9e759530b9e90a00f166
David Potapkin <davidosimo1@gmail.com>
2019-01-31 13:49:09 +0200

    refactor/requestToServer: delete extra function from this file

--- src/requestToServer.js
+++ src/requestToServer.js
@@ -1,34 +1,5 @@
 /* eslint-disable */
 
-function events(xmlRequest, progressEvent) {
-  if (progressEvent === 'upload') {
-    xmlRequest.upload.onprogress = event => {
-      loadBarProgress('upload-progress-bar', event.loaded, event.total);
-      console.log('---', `Upload to the server ${event.loaded} bytes from ${event.total}`);
-    };
-
-    xmlRequest.upload.onload = () => {
-      console.log('---', 'Data is fully uploaded to the server');
-    };
-
-    xmlRequest.upload.onerror = () => {
-      console.log('---', 'An error occurred while loading data to the server!');
-    };
-  } else if (progressEvent === 'download') {
-    xmlRequest.onprogress = event => {
-      loadBarProgress('download-progress-bar', event.loaded, event.total);
-      console.log(`Received from ${event.loaded} server bytes from ${event.total} `);
-    };
-  }
-}
-
-function getImgUrl(binaryImg) {
-  const blob = new Blob([binaryImg], { type: 'image/jpeg' });
-  const imageUrl = URL.createObjectURL(blob);
-
-  return imageUrl;
-}
-
 function uploadToServer(request, form) {
   const fileName = document.querySelector('.getFile').files[0].name;
   document.querySelector('.file-input').value = fileName;
@@ -36,33 +7,15 @@ function uploadToServer(request, form) {
 
   request.post('/upload', {
     data: form,
-    onUploadProgress: events
+    onUploadProgress
   })
     .then(response => {
       console.log('---', `Well done - ${response}`);
-      // showFilesList([response]);
     })
     .catch(e => {
       console.log(e);
     });
-
-  // request.get('/list', {}).then(data => showFilesList(JSON.parse(data)));
-    
 }
-
-function downloadFile(blob, fileName) {
-  const a = document.createElement('a');
-  const url = URL.createObjectURL(blob);
-
-  document.body.appendChild(a);
-  a.style = 'display: none';
-  a.href = url;
-  a.download = fileName;
-  a.click();
-
-  URL.revokeObjectURL(url);
-}
-
 
 function downloadFromServer(request) {
   const dataOfFile = document.querySelector('.file-input').value;
@@ -70,11 +23,11 @@ function downloadFromServer(request) {
 
   request.get(`/files/${dataOfFile}`, {
     responseType: 'blob',
-    onDownloadProgress: events
+    onDownloadProgress
   })
     .then(response => {
       document.querySelector('.info-text').innerHTML = `File ${dataOfFile} downloaded`;
-      return response.type === "image/jpeg" ? img.setAttribute('src', getImgUrl(response)) : downloadFile(response, dataOfFile);
+      return response.type === 'image/jpeg' ? img.setAttribute('src', getImgUrl(response)) : downloadFile(response, dataOfFile);
     })
     .catch(e => {
       document.querySelector('.info-text').innerHTML = `No such ${dataOfFile} file in directory - ${e}`;
