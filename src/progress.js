b0c2762fb496ff6ac89c9cef811bd8cae7bb0782
David Potapkin <davidosimo1@gmail.com>
2019-01-31 13:26:33 +0200

    refactor/progress: > delete all extra things > add function onUploadProgress > add function onDownloadProgress > create all functionality in loadBarProrgess function

--- src/progress.js
+++ src/progress.js
@@ -1,154 +1,36 @@
 /* eslint-disable */
 
-function loadBarProgress(nameOfBar, loaded, total) {
-  const progressBar = document.querySelector(`#${nameOfBar}`);
-  const title = document.querySelector('title');
+const uploadBar = document.querySelector(`#upload-progress-bar`);
+const downloadBar = document.querySelector(`#download-progress-bar`);
 
-  title.innerHTML = `Request  ${Math.floor((loaded / total) * 100)}%`;
+function loadBarProgress(event, bar) {
+  const { loaded,  total} = event;
+
+  if (bar === downloadBar) {
+    const title = document.querySelector('title');
+    title.innerHTML = `Request  ${Math.floor((loaded / total) * 100)}%`;
+    
+    setTimeout(() => {
+      if (bar === downloadBar) {
+        title.innerHTML = 'Request';
+      }
+    }, 1100);
+  }
 
   if (loaded === total) {
-    progressBar.style.opacity = '1';
-    progressBar.style.width = '100%';
+    bar.style.opacity = '1';
+    bar.style.width = '100%';
     setTimeout(() => {
-      title.innerHTML = 'Request';
-      progressBar.style.opacity = '0';
-      progressBar.style.width = '0%';
+      bar.style.opacity = '0';
+      bar.style.width = '0%';
     }, 1000);
   }
 }
 
-function showFilesList(data) {
-  const block = document.querySelector('.show-block');
-  const showList = document.querySelector('.show-list');
-  const list = document.createElement('ul');
-
-  list.className = 'list';
-
-  document.querySelector('.show-block').style.display = 'block';
-  showList.innerHTML = 'Close list';
-  showList.style.color = '#de0a4e';
-  showList.style.borderColor = '#de0a4e';
-
-  createListElement(data, list);
-  dragAndDrop(block);
-
-  block.appendChild(list);
+function onUploadProgress (event) {
+  loadBarProgress(event, uploadBar);
 }
 
-function createListElement(data, list) {
-  data.forEach(element => {
-    const listItem = document.createElement('li');
-    const link = document.createElement('a');
-
-    link.innerHTML = element;
-    link.style.color = '#fff';
-    link.style.wordWrap = 'break-word';
-    link.setAttribute('href', 'javascript:void(0);');
-
-    list.appendChild(listItem);
-    listItem.appendChild(link);
-  });
+function onDownloadProgress (event) {
+  loadBarProgress(event, downloadBar);
 }
-
-document.querySelector('.file-input').oninput = function(e) {
-  const downloadBtn = document.querySelector('.download-button');
-
-  if (!e.target.value) {
-    downloadBtn.disabled = true;
-  } else {
-    downloadBtn.disabled = false;
-  }
-};
-
-function dragAndDrop(elem) {
-  elem.onmousedown = function(e) {
-    const coords = getCoords(elem);
-    const shiftX = e.pageX - coords.left;
-    const shiftY = e.pageY - coords.top;
-
-    elem.style.position = 'absolute';
-    document.body.appendChild(elem);
-    moveAt(e);
-
-    elem.style.zIndex = 1000;
-
-    function moveAt(e) {
-      elem.style.left = `${e.pageX - shiftX}px`;
-      elem.style.top = `${e.pageY - shiftY}px`;
-    }
-
-    document.onmousemove = function(e) {
-      moveAt(e);
-    };
-
-    elem.onmouseup = function() {
-      document.onmousemove = null;
-      elem.onmouseup = null;
-    };
-  };
-
-  elem.ondragstart = function() {
-    return false;
-  };
-
-  function getCoords(el) {
-    const box = el.getBoundingClientRect();
-
-    return {
-      top: box.top + pageYOffset,
-      left: box.left + pageXOffset
-    };
-  }
-}
-
-function checkForUpload() {
-  const file = document.querySelector('.getFile').files;
-  const uploadBtn = document.querySelector('.upload-button');
-
-  if (file.length > 0) {
-    uploadBtn.disabled = false;
-  } else {
-    uploadBtn.disabled = true;
-  }
-}
-
-document.querySelector('.getFile').onchange = function(e) {
-  document.querySelector('.js-fileName').innerHTML = e.target.value.replace(/.*\\/, '');
-
-  checkForUpload();
-};
-
-const request = new HttpRequest({
-  baseUrl: 'http://localhost:8000'
-});
-
-document.getElementById('uploadForm').onsubmit = function(e) {
-  e.preventDefault();
-
-  const form = new FormData();
-  const myHeaders = new Headers();
-
-  myHeaders.append('Content-Type', 'multipart/form-data');
-  form.append('sampleFile', e.target.sampleFile.files[0]);
-
-  uploadToServer(request, form);
-
-  document.querySelector('.getFile').value = '';
-  document.querySelector('.js-fileName').innerHTML = 'Choose a file';
-  document.querySelector('.upload-button').disabled = true;
-};
-
-document.getElementById('downloadForm').onsubmit = function(e) {
-  e.preventDefault();
-
-  downloadFromServer(request);
-
-  document.querySelector('.file-input').value = '';
-  document.querySelector('.download-button').disabled = true;
-};
-
-document.querySelector('.show-list').addEventListener('click', () => {
-  getListOfFile(request);
-});
-
-checkForUpload();
