39277d1dbc15db2c0f2c40c90fd54edf081bf134
David Potapkin <davidosimo1@gmail.com>
2019-01-31 14:18:59 +0200

    improve/hepler: move functions "dragAndDrop" and "checkForUpload" from main.js to helper.js

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 function getImgUrl(binaryImg) {
   const blob = new Blob([binaryImg], { type: 'image/jpeg' });
   const imageUrl = URL.createObjectURL(blob);
@@ -25,3 +27,56 @@ const areElementsFunction = arrayOfFunction => arrayOfFunction.every(value => {
     throw new TypeError(`${value} isn't a function`);
   }
 });
+
+function checkForUpload() {
+  const file = document.querySelector('.getFile').files;
+  const uploadBtn = document.querySelector('.upload-button');
+
+  if (file.length > 0) {
+    uploadBtn.disabled = false;
+  } else {
+    uploadBtn.disabled = true;
+  }
+}
+
+
+function dragAndDrop(elem) {
+  elem.onmousedown = function(e) {
+    const coords = getCoords(elem);
+    const shiftX = e.pageX - coords.left;
+    const shiftY = e.pageY - coords.top;
+
+    elem.style.position = 'absolute';
+    document.body.appendChild(elem);
+    moveAt(e);
+
+    elem.style.zIndex = 1000;
+
+    function moveAt(e) {
+      elem.style.left = `${e.pageX - shiftX}px`;
+      elem.style.top = `${e.pageY - shiftY}px`;
+    }
+
+    document.onmousemove = function(e) {
+      moveAt(e);
+    };
+
+    elem.onmouseup = function() {
+      document.onmousemove = null;
+      elem.onmouseup = null;
+    };
+  };
+
+  elem.ondragstart = function() {
+    return false;
+  };
+
+  function getCoords(el) {
+    const box = el.getBoundingClientRect();
+
+    return {
+      top: box.top + pageYOffset,
+      left: box.left + pageXOffset
+    };
+  }
+}
