7595fc42d047e90c6684918c356a135ebd1b79e6
David Potapkin <davidosimo1@gmail.com>
2019-02-01 12:33:57 +0200

    eslint/progress: fix eslint errors

--- src/progress.js
+++ src/progress.js
@@ -1,35 +1,33 @@
-/* eslint-disable */
-
-const uploadBar = document.querySelector(`#upload-progress-bar`);
-const downloadBar = document.querySelector(`#download-progress-bar`);
+const uploadBar = document.querySelector('#upload-progress-bar');
+const downloadBar = document.querySelector('#download-progress-bar');
 
 function loadBarProgress(event, bar) {
-  const { loaded,  total} = event;
+  const { loaded, total } = event;
 
   if (bar === downloadBar) {
     const title = document.querySelector('title');
     title.innerHTML = `Request  ${Math.floor((loaded / total) * 100)}%`;
-    
+
     setTimeout(() => {
       title.innerHTML = 'Request';
     }, 1000);
   }
 
   if (loaded === total) {
-    bar.classList.remove('hide')
+    bar.classList.remove('hide');
     bar.classList.add('show');
 
     setTimeout(() => {
       bar.classList.add('hide');
-      bar.classList.remove('show')
+      bar.classList.remove('show');
     }, 1000);
   }
 }
 
-function onUploadProgress (event) {
+function onUploadProgress(event) {
   loadBarProgress(event, uploadBar);
 }
 
-function onDownloadProgress (event) {
+function onDownloadProgress(event) {
   loadBarProgress(event, downloadBar);
 }
