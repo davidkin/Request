8d4ecc9d5faa7c1d6877022a43f3be91bfaab413
David Potapkin <davidosimo1@gmail.com>
2019-02-05 02:55:01 +0200

    improve/main: add vars for DOM elements, rewrite funciton for uploadForm, downloadForm

--- src/main.js
+++ src/main.js
@@ -1,50 +1,96 @@
-/* eslint-disable */
+/* global  dragAndDrop, HttpRequest, ListOfFiles, downloadFromServer,
+onUploadProgress, onDownloadProgress, helper */
 
-const request = new HttpRequest({
-  baseUrl: 'http://localhost:8000'
-});
+const {
+  getImgUrl,
+  areElementsFunction,
+  downloadFile,
+  checkForUpload,
+  downloadBtnIsEnable,
+  isShow,
+  showResponse,
+  showError
+} = helper;
+
+const showBlock = document.querySelector('.show-block');
+
+const request = new HttpRequest({ baseUrl: 'http://localhost:8000' });
+const fileList = new ListOfFiles(showBlock);
+
+const inputGetFile = document.querySelector('.getFile');
+const inputFile = document.querySelector('.file-input');
+const spanFileName = document.querySelector('.js-fileName');
+
+const uploadButton = document.querySelector('.upload-button');
+const dowloadButton = document.querySelector('.download-button');
+const showListButton = document.querySelector('.show-list');
+
+function closeFileList() {
+  fileList.remove();
+
+  showListButton.innerHTML = 'Show List';
+
+  showListButton.classList.toggle('red');
+  showListButton.classList.toggle('active');
+
+  showBlock.classList.toggle('block');
+}
 
 document.getElementById('uploadForm').onsubmit = function(e) {
   e.preventDefault();
 
   const form = new FormData();
-  const myHeaders = new Headers();
 
-  myHeaders.append('Content-Type', 'multipart/form-data');
   form.append('sampleFile', e.target.sampleFile.files[0]);
 
-  uploadToServer(request, form);
+  downloadBtnIsEnable();
 
-  document.querySelector('.getFile').value = '';
-  document.querySelector('.js-fileName').innerHTML = 'Choose a file';
-  document.querySelector('.upload-button').disabled = true;
+  request.post('/upload', { data: form, onUploadProgress })
+    .then(response => console.log('---', `Well done - ${response}`)) // eslint-disable-line no-console
+    .catch(e => console.log(e)); // eslint-disable-line no-console
+
+  inputGetFile.value = '';
+  spanFileName.innerHTML = 'Choose a file';
+  uploadButton.disabled = true;
 };
 
 document.getElementById('downloadForm').onsubmit = function(e) {
   e.preventDefault();
 
-  downloadFromServer(request);
+  const fileName = document.querySelector('.file-input').value;
 
-  document.querySelector('.file-input').value = '';
-  document.querySelector('.download-button').disabled = true;
+  request.get(`/files/${fileName}`, { responseType: 'blob', onDownloadProgress })
+    .then(response => showResponse(fileName, response))
+    .catch(error => showError(error.statusText, fileName));
+
+  inputFile.value = '';
+  dowloadButton.disabled = true;
 };
 
-document.querySelector('.show-list').onclick= function (e) {
-  getListOfFile(request);
+document.querySelector('.show-list').onclick = function(e) {
+  if (isShow()) {
+    showListButton.innerHTML = 'Close list';
+    showListButton.classList.toggle('active');
+
+    showBlock.classList.toggle('block');
+    showListButton.classList.toggle('red');
+
+    return fileList.create();
+  }
+
+  return closeFileList();
 };
 
 document.querySelector('.file-input').oninput = function(e) {
-  const downloadBtn = document.querySelector('.download-button');
-
   if (!e.target.value) {
-    downloadBtn.disabled = true;
+    dowloadButton.disabled = true;
   } else {
-    downloadBtn.disabled = false;
+    dowloadButton.disabled = false;
   }
 };
 
 document.querySelector('.getFile').onchange = function(e) {
-  document.querySelector('.js-fileName').innerHTML = e.target.value.replace(/.*\\/, '');
+  spanFileName.innerHTML = e.target.value.replace(/.*\\/, '');
 
   checkForUpload();
 };
