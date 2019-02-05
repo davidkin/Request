9f77e2db29f373ce76a2733813265afaef5b3fb6
David Potapkin <davidosimo1@gmail.com>
2019-02-05 02:37:06 +0200

    fix/progress: chane ID on classes for progress bars and delete extra setTimout

--- src/progress.js
+++ src/progress.js
@@ -1,16 +1,12 @@
-const uploadBar = document.querySelector('#upload-progress-bar');
-const downloadBar = document.querySelector('#download-progress-bar');
+const uploadBar = document.querySelector('.upload-progress-bar');
+const downloadBar = document.querySelector('.download-progress-bar');
 
 function loadBarProgress(event, bar) {
   const { loaded, total } = event;
+  const title = document.querySelector('title');
 
   if (bar === downloadBar) {
-    const title = document.querySelector('title');
     title.innerHTML = `Request  ${Math.floor((loaded / total) * 100)}%`;
-
-    setTimeout(() => {
-      title.innerHTML = 'Request';
-    }, 1000);
   }
 
   if (loaded === total) {
@@ -18,6 +14,8 @@ function loadBarProgress(event, bar) {
     bar.classList.add('show');
 
     setTimeout(() => {
+      title.innerHTML = 'Request';
+
       bar.classList.add('hide');
       bar.classList.remove('show');
     }, 1000);
