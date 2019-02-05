c1fa861b68549d0f760514314ce889e4a539516c
David Potapkin <davidosimo1@gmail.com>
2019-02-05 02:29:12 +0200

    fix/fileList: replace node redefinition into render function

--- src/fileList.js
+++ src/fileList.js
@@ -18,19 +18,22 @@
       this.parentNode = parentNode;
       this.data = [];
       this.node = null;
+
+      this.create();
     }
 
     create() {
-      this.loadList().then(() => this.render);
-      this.node = this.parentNode.firstElementChild;
+      this.loadList().then(() => this.render());
+      dragAndDrop(this.parentNode);
     }
 
     render() {
       this.parentNode.innerHTML = render(this.data);
+      this.node = this.parentNode.firstElementChild;
     }
 
     loadList() {
-      request.get('/list', {}).then(data => (this.data = data));
+      return request.get('/list', {}).then(data => (this.data = JSON.parse(data)));
     }
 
     remove() {
@@ -40,53 +43,3 @@
 
   window.ListOfFiles = ListOfFiles;
 }());
-// dragAndDrop(showBlock);
-
-// function showFilesList(data) {
-//   const showBlock = document.querySelector('.show-block');
-//   const showList = document.querySelector('.show-list');
-
-//   const list = document.createElement('ul');
-//   list.className = 'list';
-
-//   showList.innerHTML = 'Close list';
-
-//   showBlock.classList.remove('delete');
-//   showList.classList.remove('white');
-
-//   showBlock.classList.add('block');
-//   showList.classList.add('red');
-
-//   createListElements(data, list);
-//
-
-//   showBlock.appendChild(list);
-// }
-
-// function createListElements(data, list) {
-//   data.forEach(element => {
-//     const listItem = document.createElement('li');
-//     const link = document.createElement('span');
-//     link.classList.add('list-text');
-
-//     link.innerHTML = element;
-
-//     list.appendChild(listItem);
-//     listItem.appendChild(link);
-//   });
-// }
-
-// function closeFileList() {
-//   const showBlock = document.querySelector('.show-block');
-//   const showList = document.querySelector('.show-list');
-
-//   showBlock.removeChild(document.querySelector('.list'));
-
-//   showList.classList.remove('red');
-//   showBlock.classList.remove('block');
-
-//   showBlock.classList.add('delete');
-//   showList.classList.add('white');
-
-//   showList.innerHTML = 'Show List';
-// }
